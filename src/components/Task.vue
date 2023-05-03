ac1b0248fbdc8608323289265f508eda6388c342
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-05-03 07:40:10 +0300

    Able to pass down binded props and loop through to display lists

---
+++ src/components/Task.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  props: {
+    task: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="task">
+    <h3>{{ task.text }}</h3>
+    <p>{{ task.day }}</p>
+  </div>
+</template>
+
+<style>
+.fas {
+  color: red;
+}
+
+.task {
+  background: #f4f4f4;
+  margin: 5px;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+
+.task.reminder {
+  border-left: 5px solid green;
+}
+
+.task h3 {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<script>
+import Task from "./Task.vue"
+    export default {
+        name: "Tasks",
+        props: {
+            tasks: Array
+        },
+        components: {
+            Task
+        }
+    }
+</script>
+
+<template>
+    <div v-for="task in tasks">
+        <Task :task="task" />
+    </div>
+</template>
